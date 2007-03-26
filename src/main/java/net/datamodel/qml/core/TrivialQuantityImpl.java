b23881aa03d2dba6b85a58c7d4fe19f6990476a6
thomas <>
2007-03-26 20:04:35 +0000

    messing with interface. now inherits from componentImpl rather than AQImpl. More work to be done..

--- src/main/java/net/datamodel/qml/core/TrivialQuantityImpl.java
+++ src/main/java/net/datamodel/qml/core/TrivialQuantityImpl.java
@@ -31,8 +31,8 @@ package net.datamodel.qml.core;
 
 import java.io.IOException;
 import java.io.Writer;
-import java.util.Hashtable;
-import java.util.Vector;
+import java.util.List;
+import java.util.Map;
 
 import net.datamodel.qml.DataType;
 import net.datamodel.qml.Locator;
@@ -50,148 +50,158 @@ import net.datamodel.xssp.XMLSerializableObject;
  * string value.
  */
 public class TrivialQuantityImpl 
-extends AtomicQuantityImpl 
+extends ComponentImpl 
 {
 
-    /** No-arg constructor  */
-    public TrivialQuantityImpl () { 
-       super(-1);
-       setXMLNodeName(Constants.NodeName.TRIVIAL_QUANTITY);
-    }
+	/** No-arg constructor  */
+	public TrivialQuantityImpl () { 
+		super();
+		setXMLNodeName(Constants.NodeName.TRIVIAL_QUANTITY);
+	}
 
-    /*
-     * (non-Javadoc)
-     * @see net.datamodel.qml.core.ComponentImpl#setDataType(net.datamodel.qml.DataType)
-     */
-    @Override
-    public void setDataType (DataType value) 
-    throws IllegalArgumentException
-    {
-        if (value instanceof StringDataType) {
-           super.setDataType(value);
-        } else {
-           throw new IllegalArgumentException("setDataType other than StringDataType not allowed in Trivial Quantity");
-        }
-    }
+	/*
+	 * (non-Javadoc)
+	 * @see net.datamodel.qml.core.ComponentImpl#setDataType(net.datamodel.qml.DataType)
+	 */
+	@Override
+	public final void setDataType (DataType value) 
+	throws IllegalArgumentException
+	{
+		if (value instanceof StringDataType) {
+			super.setDataType(value);
+		} else {
+			throw new IllegalArgumentException("setDataType other than StringDataType not allowed in Trivial Quantity");
+		}
+	}
 
-    /*
-     * (non-Javadoc)
-     * @see net.datamodel.qml.core.ComponentImpl#setUnits(net.datamodel.qml.Units)
-     */
-    public void setUnits ( Units value ) 
-    throws IllegalArgumentException
-    {
-        if (value.getString().equals("")) {
-           super.setUnits(value);
-        } else {
-           throw new IllegalArgumentException("setUnits other than Unitless not allowed in Trivial Quantity");
-        }
-    }
+	/*
+	 * (non-Javadoc)
+	 * @see net.datamodel.qml.core.ComponentImpl#setUnits(net.datamodel.qml.Units)
+	 */
+	public final void setUnits ( Units value ) 
+	throws IllegalArgumentException
+	{
+		if (value.getString().equals("")) {
+			super.setUnits(value);
+		} else {
+			throw new IllegalArgumentException("setUnits other than Unitless not allowed in Trivial Quantity");
+		}
+	}
 
-  /*
-   * (non-Javadoc)
-   * @see net.datamodel.qml.core.AtomicQuantityImpl#setValue(java.lang.Byte, net.datamodel.qml.Locator)
-   */
-    public void setValue (Byte obj, Locator loc)
-       throws IllegalAccessException, IllegalArgumentException, NullPointerException, SetDataException
-    {
-        throw new IllegalAccessException("Byte data not allowed in Trivial Quantity");
-    }
+	/*
+	 * (non-Javadoc)
+	 * @see net.datamodel.qml.core.AtomicQuantityImpl#setValue(java.lang.Byte, net.datamodel.qml.Locator)
+	 */
+	public void setValue (Byte obj, Locator loc)
+	throws IllegalAccessException, IllegalArgumentException, NullPointerException, SetDataException
+	{
+		throw new IllegalAccessException("Byte data not allowed in Trivial Quantity");
+	}
 
-    /*
-     * (non-Javadoc)
-     * @see net.datamodel.qml.core.AtomicQuantityImpl#setValue(java.lang.Double, net.datamodel.qml.Locator)
-     */
-    public void setValue (Double obj, Locator loc)
-       throws IllegalAccessException, IllegalArgumentException, NullPointerException, SetDataException
-    {
-        throw new IllegalAccessException("Double data not allowed in Trivial Quantity");
-    }
+	/*
+	 * (non-Javadoc)
+	 * @see net.datamodel.qml.core.AtomicQuantityImpl#setValue(java.lang.Double, net.datamodel.qml.Locator)
+	 */
+	public void setValue (Double obj, Locator loc)
+	throws IllegalAccessException, IllegalArgumentException, NullPointerException, SetDataException
+	{
+		throw new IllegalAccessException("Double data not allowed in Trivial Quantity");
+	}
 
-    /*
-     * (non-Javadoc)
-     * @see net.datamodel.qml.core.AtomicQuantityImpl#setValue(java.lang.Integer, net.datamodel.qml.Locator)
-     */
-    public void setValue (Integer obj, Locator loc)
-       throws IllegalAccessException, IllegalArgumentException, NullPointerException, SetDataException
-    {
-        throw new IllegalAccessException("Integer data not allowed in Trivial Quantity");
-    }
+	/*
+	 * (non-Javadoc)
+	 * @see net.datamodel.qml.core.AtomicQuantityImpl#setValue(java.lang.Integer, net.datamodel.qml.Locator)
+	 */
+	public void setValue (Integer obj, Locator loc)
+	throws IllegalAccessException, IllegalArgumentException, NullPointerException, SetDataException
+	{
+		throw new IllegalAccessException("Integer data not allowed in Trivial Quantity");
+	}
 
-    /*
-     * (non-Javadoc)
-     * @see net.datamodel.qml.core.AtomicQuantityImpl#setValue(java.lang.Short, net.datamodel.qml.Locator)
-     */
-    public void setValue (Short obj, Locator loc)
-       throws IllegalAccessException, IllegalArgumentException, NullPointerException, SetDataException
-    {
-        throw new IllegalAccessException("Short data not allowed in Trivial Quantity");
-    }
+	/*
+	 * (non-Javadoc)
+	 * @see net.datamodel.qml.core.AtomicQuantityImpl#setValue(java.lang.Short, net.datamodel.qml.Locator)
+	 */
+	public void setValue (Short obj, Locator loc)
+	throws IllegalAccessException, IllegalArgumentException, NullPointerException, SetDataException
+	{
+		throw new IllegalAccessException("Short data not allowed in Trivial Quantity");
+	}
 
-    // FIXME : we are overriding a method in XSSP package!!
-    /** returns true if there were child nodes to handle */
-    @Override
-    protected boolean handleChildNodes(Hashtable idTable, Hashtable prefixTable, Writer outputWriter, String nodeNameString,
-                                       String indent, Vector childObjs, XMLSerializableField PCDATA)
-    throws IOException
-    {
+	/** Returns true if there were child nodes to handle.
+	 * 
+	 */
+	// FIXME : we are overriding a method in XSSP package, bah!!
+	@Override
+	protected boolean handleChildNodes(Map<String,String> idTable, 
+			Map<XMLSerializableObject, String> prefixTable, 
+			Writer outputWriter, 
+			String nodeNameString,
+			String indent, 
+			List<XMLSerializableObject> childObjs, 
+			XMLSerializableField PCDATA)
+	throws IOException
+	{
 
-       if ( childObjs.size() > 0 )
-       {
+		if ( childObjs.size() > 0 )
+		{
 
-          Object value = null;
+			Object value = null;
 
-          // deal with child object/list XML fields, if any in our list
-          int objs_size = childObjs.size();
-          for (int i = 0; i < objs_size; i++) {
-            XMLSerializableField field = (XMLSerializableField) childObjs.get(i);
+			// deal with child object/list XML fields, if any in our list
+			int objs_size = childObjs.size();
+			for (int i = 0; i < objs_size; i++) {
+				XMLSerializableField field = (XMLSerializableField) childObjs.get(i);
 
-            if (field.getType() == XMLFieldType.CHILD)
-            {
+				if (field.getType() == XMLFieldType.CHILD)
+				{
 
-               XMLSerializableObject containedObj = (XMLSerializableObject) field.getValue();
-               if(containedObj instanceof ValueContainer)
-               {
-                   value = ((ValueContainer) containedObj).getFirstValue();
-                   break;
-               }
+					XMLSerializableObject containedObj = (XMLSerializableObject) field.getValue();
+					if(containedObj instanceof ValueContainer)
+					{
+						value = ((ValueContainer) containedObj).getFirstValue();
+						break;
+					}
 
-            } else {
-               // do nothing with other children
-            }
+				} else {
+					// do nothing with other children
+				}
 
-          }
+			}
 
-          // close the opening tag, print our value
-          if (nodeNameString != null && value != null) {
-             if(!nodeNameString.equals(""))
-                outputWriter.write(">");
+			// close the opening tag, print our value
+			if (nodeNameString != null && value != null) {
+				if(!nodeNameString.equals(""))
+					outputWriter.write(">");
 
-             outputWriter.write(value.toString());
-             return true;
-          }
+				outputWriter.write(value.toString());
+				return true;
+			}
 
-        }
+		}
 
-        return false;
-    }
-    
-    // FIXME: we are overriding a method which is in the XSSP package (!!)
-    @Override
-    protected void doClosingNodeForChildren (String nodeNameString, 
-    										String indent, boolean hasPCDATA,
-                                             boolean isPrettyOutput,  Writer outputWriter)
-    throws IOException
-    {
+		return false;
+	}
 
-          // ok, now deal with closing the node
-          if (nodeNameString != null && !nodeNameString.equals("")) {
+	// FIXME: we are overriding a method which is in the XSSP package (!!)
+	@Override
+	protected void doClosingNodeForChildren (
+			String nodeNameString, 
+			String indent, boolean hasPCDATA,
+			boolean isPrettyOutput,  
+			Writer outputWriter
+	)
+	throws IOException
+	{
 
-               if(!nodeNameString.equals(""))
-                   outputWriter.write("</"+nodeNameString+">");
-          }
+		// ok, now deal with closing the node
+		if (nodeNameString != null && !nodeNameString.equals("")) {
 
-    }
+			if(!nodeNameString.equals(""))
+				outputWriter.write("</"+nodeNameString+">");
+		}
+
+	}
 
 }
 
