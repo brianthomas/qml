874be926235f5c2e886a766541bcc9fa2038661d
thomas <>
2007-03-29 16:55:39 +0000

    clean up imports, docs and remove final from overridden methods

--- src/main/java/net/datamodel/qml/support/DOMXerces2/QMLElementImpl.java
+++ src/main/java/net/datamodel/qml/support/DOMXerces2/QMLElementImpl.java
@@ -31,21 +31,13 @@
 package net.datamodel.qml.support.DOMXerces2;
 
 import java.io.IOException;
-import java.io.Writer;
-import java.util.Map;
 
 import net.datamodel.qml.Quantity;
-import net.datamodel.qml.support.Constants;
 import net.datamodel.qml.support.QMLElement;
-import net.datamodel.xssp.ReferenceableXMLSerializableObject;
-import net.datamodel.xssp.XMLSerializableField;
-import net.datamodel.xssp.XMLSerializableObject;
-import net.datamodel.xssp.XMLSerializableObjectWithFields;
+import net.datamodel.soml.support.DOMXerces2.SOMLElementImpl;
 
 import org.apache.log4j.Logger;
 import org.apache.xerces.dom.DocumentImpl;
-import org.apache.xerces.dom.ElementNSImpl;
-import org.w3c.dom.Attr;
 import org.w3c.dom.Comment;
 import org.w3c.dom.DOMException;
 import org.w3c.dom.Node;
@@ -53,227 +45,42 @@ import org.w3c.dom.Node;
 /** An implementation of QMLElement based on the Xerces2 ElementNSImpl class.
  */
 
-public class QMLElementImpl extends ElementNSImpl
+public class QMLElementImpl extends SOMLElementImpl 
 implements QMLElement
 {
 
 	private static final Logger logger = Logger.getLogger(QMLElementImpl.class);
 
 	private static final long serialVersionUID = -4652785809392671264L;
+	
+	public QMLElementImpl(Quantity q, DocumentImpl doc) 
+	throws IOException, NullPointerException 
+	{
+		super(q, doc);
+	}
 
-
-	Quantity myQuantity = null;
-
-	//
-	// Constructors
-	//
-
-	// hmm..badness. We expect Quantity interface, then demand (internally) the 
-	// XMLSerializableObject interface. This could lead to problems down the
-	// line. :P 
+	/*
 	public QMLElementImpl (String namespaceURI, Quantity quantity, DocumentImpl doc) 
 	throws IOException,NullPointerException
 	{
-		super (doc, namespaceURI, ((XMLSerializableObject) quantity).getXMLTagName());
-		((XMLSerializableObject) quantity).setNamespaceURI(namespaceURI);
-		setQuantity(quantity);
+		super (doc, namespaceURI, quantity.getXMLTagName());
+		quantity.setNamespaceURI(namespaceURI);
 	}
-
-	// this has same issue as the constructor above
-	public QMLElementImpl (Quantity quantity, DocumentImpl doc) 
-	throws IOException,NullPointerException
-	{
-		super (doc, Constants.QML_NAMESPACE_URI, ((XMLSerializableObject) quantity).getXMLTagName());
-		setQuantity(quantity);
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * @see org.apache.xerces.dom.ElementImpl#getAttribute(java.lang.String)
-	 */
-	@Override
-	public final String getAttribute(String name) 
-	{
-		XMLSerializableField field = ((XMLSerializableObjectWithFields) getQuantity()).getXMLSerializableField(name);
-		if (field != null)
-			return field.getValue().toString();
-		return null;
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * @see org.apache.xerces.dom.ElementImpl#getTagName()
-	 */
-	@Override
-	public final String getTagName() {
-		return ((XMLSerializableObject) getQuantity()).getXMLTagName();
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * @see org.apache.xerces.dom.ElementImpl#setAttribute(java.lang.String, java.lang.String)
-	 */
-	@Override
-	public final void setAttribute(String name, String value) 
-	{
-		
-		if (!getQuantity().addAttributeField(name, value))
-			logger.error("setAttribute not allowed on existing attribute.");
-		
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * @see org.apache.xerces.dom.ElementImpl#getAttributeNS(java.lang.String, java.lang.String)
-	 */
-	@Override
-	public final String getAttributeNS(String namespaceURI, String localName) 
-	{
-		logger.error("getAttributeNS not allowed for Xerces2DOM.QMLElementImpl");
-		return null;
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * @see org.apache.xerces.dom.ElementImpl#setAttributeNS(java.lang.String, java.lang.String, java.lang.String)
-	 */
-	@Override
-	public final void setAttributeNS ( String uri, String name, String value)
-	{
-		// not allowed
-		logger.error("setAttributeNS not allowed for Xerces2DOM.QMLElementImpl");
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * @see org.apache.xerces.dom.ElementImpl#setAttributeNode(org.w3c.dom.Attr)
-	 */
-	@Override
-	public final Attr setAttributeNode (Attr node) 
-	{
-		if (!getQuantity().addAttributeField(node.getName(), node.getValue()))
-			logger.error("setAttributeNode not allowed on existing attribute."); 
-		// FIXME: return an attribute Node from method 
-		return null;
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * @see org.apache.xerces.dom.ElementImpl#getAttributeNode(java.lang.String)
-	 */
-	@Override
-	public final Attr getAttributeNode(String name) 
-	{
-		logger.error("getAttributeNode not allowed for Xerces2DOM.QMLElementImpl");
-		return null;
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * @see org.apache.xerces.dom.ElementImpl#setAttributeNodeNS(org.w3c.dom.Attr)
-	 */
-	@Override
-	public final Attr setAttributeNodeNS ( Attr node) 
-	{
-		logger.error("setAttributeNodeNS not allowed for Xerces2DOM.QMLElementImpl");
-		return null;
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * @see org.apache.xerces.dom.ElementImpl#getAttributeNodeNS(java.lang.String, java.lang.String)
-	 */
-	@Override
-	public final Attr getAttributeNodeNS(String namespaceURI, String localName) 
-	{
-		logger.error("getAttributeNodeNS not allowed for Xerces2DOM.QMLElementImpl");
-		return null;
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * @see org.apache.xerces.dom.ElementImpl#hasAttribute(java.lang.String)
-	 */
-	@Override
-	public final boolean hasAttribute (String name) 
-	{
-		XMLSerializableField field = ((XMLSerializableObjectWithFields) getQuantity()).getXMLSerializableField(name);
-		return (field == null) ? false : true;
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * @see org.apache.xerces.dom.ElementImpl#hasAttributeNS(java.lang.String, java.lang.String)
-	 */
-	@Override
-	public final boolean hasAttributeNS (String namespaceURI, String localName) 
-	{
-		logger.error("hasAttributeNS not allowed for Xerces2DOM.QMLElementImpl");
-		return false;
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * @see org.apache.xerces.dom.ElementImpl#removeAttribute(java.lang.String)
-	 */
-	@Override
-	public final void removeAttribute(String name) 
-	{
-//		((XMLSerializableObject) getQuantity()).removeXMLSerializableField(name);
-		if (!getQuantity().removeAttributeField(name))
-			logger.error("removeAttributeNode could not remove Attribute node:"+name);
-		
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * @see org.apache.xerces.dom.ElementImpl#removeAttributeNS(java.lang.String, java.lang.String)
-	 */
-	@Override
-	public final void removeAttributeNS (String namespaceURI, String name) 
-	{
-		logger.error("removeAttributeNS not allowed for Xerces2DOM.QMLElementImpl");
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * @see org.apache.xerces.dom.ElementImpl#removeAttributeNode(org.w3c.dom.Attr)
-	 */
-	@Override
-	public final Attr removeAttributeNode (Attr oldAttr) 
-	{
-		logger.error("removeAttributeNode not allowed for Xerces2DOM.QMLElementImpl");
-
-		// FIXME: need to return an attribute node we removed?
-		return null;
-	}
+	*/
 
 	/*
 	 * (non-Javadoc)
 	 * @see net.datamodel.qml.support.QMLElement#getQuantity()
 	 */
-	public final Quantity getQuantity()  { return myQuantity; }
-
-	/** Set the Quantity which represents this element.
-	 * 
-	 * @param object
-	 * @throws NullPointerException
-	 */
-	protected final void setQuantity (Quantity object) 
-	throws NullPointerException
-	{
-		if (object == null) 
-			throw new NullPointerException("Can't set QMLElement with null Quantity object pointer");
-		else
-			myQuantity = object;
-	}
+	public final Quantity getQuantity()  { return (Quantity) getSemanticObject(); }
 
 	/*
 	 * (non-Javadoc)
 	 * @see org.apache.xerces.dom.ParentNode#appendChild(org.w3c.dom.Node, org.w3c.dom.Node)
 	 */
+	// TODO: doc on overriding this method
 	@Override
-	public final Node appendChild(Node newChild) 
+	public Node appendChild(Node newChild) 
 	throws DOMException
 	{
 
@@ -303,21 +110,22 @@ implements QMLElement
 	 * (non-Javadoc)
 	 * @see org.apache.xerces.dom.ParentNode#insertBefore(org.w3c.dom.Node, org.w3c.dom.Node)
 	 */
+	// TODO: notes on overriding this method
 	@Override
-	public final Node insertBefore(Node newChild, Node refChild) throws DOMException
+	public Node insertBefore(Node newChild, Node refChild) throws DOMException
 	{
 
 		Node node = null;
 
 		if (newChild instanceof QMLElement)
 		{
+			node = super.insertBefore(newChild,refChild);
+			
 			QMLElement qElem = (QMLElement) newChild;
 			Quantity q = qElem.getQuantity();
 
 			// Add as a member
 			getQuantity().addProperty(q);
-
-			node = super.insertBefore(newChild,refChild);
 
 		} else {
 			throw new DOMException (DOMException.NOT_SUPPORTED_ERR, "Can't remove regular DOM Element from QMLElement object.");
@@ -330,7 +138,8 @@ implements QMLElement
 	 * (non-Javadoc)
 	 * @see org.apache.xerces.dom.ParentNode#removeChild(org.w3c.dom.Node)
 	 */
-	public final Node removeChild(Node oldChild) throws DOMException
+	// TODO: notes on overriding this method
+	public Node removeChild(Node oldChild) throws DOMException
 	{
 
 		if (oldChild instanceof QMLElement)
@@ -354,7 +163,8 @@ implements QMLElement
 	 * (non-Javadoc)
 	 * @see org.apache.xerces.dom.ParentNode#replaceChild(org.w3c.dom.Node, org.w3c.dom.Node)
 	 */
-	public final Node replaceChild(Node newChild, Node oldChild) throws DOMException
+	// TODO: doc on overriding this method
+	public Node replaceChild(Node newChild, Node oldChild) throws DOMException
 	{
 		Node node = super.replaceChild(newChild, oldChild);
 
@@ -386,32 +196,7 @@ implements QMLElement
 	}
 
 	/*
-	 * (non-Javadoc)
-	 * @see net.datamodel.qml.support.QMLElement#toXMLString()
-	 */
-	public final String toXMLString () {
-		return ((XMLSerializableObject) getQuantity()).toXMLString();
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * @see net.datamodel.qml.support.QMLElement#toXMLWriter(java.io.Writer)
-	 */
-	public final void toXMLWriter (Writer outputWriter)
-	throws java.io.IOException
-	{
-		toXMLWriter(outputWriter,"",false, false);
-	}
-
-	/** */
-	private void toXMLWriter (Writer outputWriter, String indent)
-	throws java.io.IOException
-	{
-		toXMLWriter(outputWriter, indent, false, false);
-	}
-
-	/** */
-	private void toXMLWriter (Writer outputWriter, String indent, boolean doFirstIndent, boolean doLastNewLine)
+	public void toXMLWriter (Writer outputWriter, String indent, boolean doFirstIndent, boolean doLastNewLine)
 	throws java.io.IOException
 	{
 
@@ -427,11 +212,12 @@ implements QMLElement
 		{
 			Map<String,ReferenceableXMLSerializableObject> idTable = ((QMLDocumentImpl) getOwnerDocument()).getXMLSerializableObjectIdTable();
 			Map<String,String> prefixTable = ((QMLDocumentImpl) getOwnerDocument()).getPrefixNamespaceMappings();
-			((XMLSerializableObject) q).toXMLWriter(idTable, prefixTable, outputWriter, indent, doFirstIndent, doLastNewLine);
+			q.toXMLWriter(idTable, prefixTable, outputWriter, indent, doFirstIndent, doLastNewLine);
 		} else 
-			((XMLSerializableObject) q).toXMLWriter(outputWriter, indent, doFirstIndent, doLastNewLine);
+			q.toXMLWriter(outputWriter, indent, doFirstIndent, doLastNewLine);
 
 	}
+	*/
 
 }
 
