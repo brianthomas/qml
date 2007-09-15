9dd92976435fbe37eda7018d148862259dd22eb1
thomas <>
2007-09-15 17:56:21 +0000

    small fix

--- src/main/java/net/datamodel/qml/datatype/FloatDataType.java
+++ src/main/java/net/datamodel/qml/datatype/FloatDataType.java
@@ -148,8 +148,8 @@ extends NumberDataType
 	 * @see net.datamodel.qml.DataType#getFortranFormat()
 	 */
 	public final String getFortranFormat() {
-		if (getExponent() != null)
-		{
+		if (getExponent().intValue() != 0) {
+			// FIXME : this isnt right..being sloppy
 			return "E"+getWidth()+"."+getPrecision();
 		}
 		return "F"+getWidth()+"."+getPrecision();
